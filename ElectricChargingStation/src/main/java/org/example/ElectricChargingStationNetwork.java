554a246ba6feb1f822a0f7478a6c74c2f63bdc9b
JonathanVormayr <61434821+JonathanVormayr@users.noreply.github.com>
2025-11-28 19:24:17 +0100

    added create charger to main

--- ElectricChargingStation/src/main/java/org/example/ElectricChargingStationNetwork.java
+++ ElectricChargingStation/src/main/java/org/example/ElectricChargingStationNetwork.java
@@ -3,6 +3,12 @@ package org.example;
 
 public class ElectricChargingStationNetwork {
     public static void main(String[] args) {
+        LocationService locationService = new LocationService();
+        
+        locationService.createLocation("Karlsplatz Charging", "Karlsplatz 1, 1010 Wien");
+        Location location = locationService.getLocationByName("Karlsplatz Charging");
 
+        location.createCharger("AC", "FREE");
+        System.out.println(location.getAllChargersAsString());
     }
 }
