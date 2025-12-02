0234547a219ff123b662496c522aa95513962a5c
JonathanVormayr <61434821+JonathanVormayr@users.noreply.github.com>
2025-12-02 16:44:20 +0100

    Revert "frst steps"
    This reverts commit 0b24dcd1ff3c460beecbf9111c553aebcad2c239.

--- ElectricChargingStation/src/main/java/org/example/InvoiceItem.java
+++ ElectricChargingStation/src/main/java/org/example/InvoiceItem.java
@@ -1,11 +1,4 @@
 package org.example;
 
-import java.time.LocalDate;
-
-public abstract class InvoiceItem {
-    private LocalDate invoiceDate;
-
-    public  InvoiceItem(LocalDate invoiceDate){
-        this.invoiceDate = invoiceDate;
-    }
+public class InvoiceItem {
 }
--- ElectricChargingStation/src/test/java/org/example/ManageBalanceStepdefs.java
+++ ElectricChargingStation/src/test/java/org/example/ManageBalanceStepdefs.java
@@ -7,21 +7,15 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class ManageBalanceStepdefs {
-    CustomerManager customerManager = new CustomerManager();
     //region background
     @Given("customer {string} with the email {string} exists")
     public void customerWithTheEmailExists(String name, String email) {
-        customerManager.createCustomer(name, email);
+        throw new PendingException();
     }
 
     @And("the customer {string} hast id {int}")
     public void theCustomerHastId(String name, int id) {
-        Customer foundCustomer = customerManager.customerRepo.values()
-                    .stream()
-                    .filter(customer -> customer.getName().equals(name))
-                    .findFirst()
-                    .orElse(null);
-
+        throw new PendingException();
     }
 
     @Given("customer with id {int} has {int} invoices")
