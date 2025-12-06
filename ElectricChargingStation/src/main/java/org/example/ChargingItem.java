0445f73612d56434b7bb7bc8716d2923c5462de8
JulianRainer <wi24b045@technikum-wien.at>
2025-12-06 12:54:31 +0100

    US-5.1: feedback adaptation
    tried to use exitsting methods for the toString method.
    tests still run through

--- ElectricChargingStation/src/main/java/org/example/ChargingItem.java
+++ ElectricChargingStation/src/main/java/org/example/ChargingItem.java
@@ -64,15 +64,14 @@ public class ChargingItem extends InvoiceItem {
         Price price = location.priceList.get(priceId);
         Charger charger = location.chargersRepo.get(chargerId);
 
+        double energyPrice = chargerType == ChargerType.AC ? price.price_per_kWh_AC : price.price_per_kWh_DC;
+        double parkingPrice = chargerType == ChargerType.AC ? price.parking_pricePerHour_AC : price.parking_pricePerHour_DC;
         double energyKwh = (duration / 60.0) *
                 (chargerType == ChargerType.AC ? charger.getAC_Kw() : charger.getDC_Kw());
 
-        double energyPrice = chargerType == ChargerType.AC ? price.price_per_kWh_AC : price.price_per_kWh_DC;
-        double parkingPrice = chargerType == ChargerType.AC ? price.parking_pricePerHour_AC : price.parking_pricePerHour_DC;
-
-        double energyCost = energyKwh * energyPrice;
-        double parkingCost = (duration / 60.0) * parkingPrice;
-        double total = -(energyCost + parkingCost);
+        double chargingCost = calculateChargingCost(price, charger);
+        double parkingCost = calculateParkingCosts(price);
+        double total = getInvoiceValue();
 
         return String.format(
                 "%d - CHARGE - duration: %dmin - energy: %.2fkWh - price_per_kWh_%s: %.2f - parking_price_%s: %.2f - energy_cost: %.2f - parking_cost: %.2f - total: %.2f",
@@ -83,7 +82,7 @@ public class ChargingItem extends InvoiceItem {
                 energyPrice,
                 chargerType.name(),
                 parkingPrice,
-                energyCost,
+                chargingCost,
                 parkingCost,
                 total
         );
